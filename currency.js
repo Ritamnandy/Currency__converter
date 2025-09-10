4dce4c39ae3d730f6729b20006fe1bba59807cd7
Ritam Nandy <nandyritam519@gmail.com>
2025-09-10 11:10:24 +0530

    added index.html

---
+++ currency.js
@@ -0,0 +1,408 @@
+let country_list = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+let currency_fullform = {
+  AED: "United Arab Emirates Dirham",
+  AFN: "Afghan Afghani",
+  XCD: "East Caribbean Dollar",
+  ALL: "Albanian Lek",
+  AMD: "Armenian Dram",
+  ANG: "Netherlands Antillean Guilder",
+  AOA: "Angolan Kwanza",
+  ARS: "Argentine Peso",
+  AUD: "Australian Dollar",
+  AZN: "Azerbaijani Manat",
+  BAM: "Bosnia and Herzegovina Convertible Mark",
+  BBD: "Barbadian Dollar",
+  BDT: "Bangladeshi Taka",
+  BGN: "Bulgarian Lev",
+  BHD: "Bahraini Dinar",
+  BIF: "Burundian Franc",
+  BMD: "Bermudian Dollar",
+  BND: "Brunei Dollar",
+  BOB: "Bolivian Boliviano",
+  BRL: "Brazilian Real",
+  BSD: "Bahamian Dollar",
+  BWP: "Botswana Pula",
+  BYR: "Belarusian Ruble (old)",
+  BZD: "Belize Dollar",
+  CAD: "Canadian Dollar",
+  CDF: "Congolese Franc",
+  CHF: "Swiss Franc",
+  CLP: "Chilean Peso",
+  CNY: "Chinese Yuan",
+  COP: "Colombian Peso",
+  CRC: "Costa Rican Colón",
+  CUP: "Cuban Peso",
+  CVE: "Cape Verdean Escudo",
+  CYP: "Cypriot Pound (old)",
+  CZK: "Czech Koruna",
+  DJF: "Djiboutian Franc",
+  DKK: "Danish Krone",
+  DOP: "Dominican Peso",
+  DZD: "Algerian Dinar",
+  EGP: "Egyptian Pound",
+  ERN: "Eritrean Nakfa",
+  ETB: "Ethiopian Birr",
+  EUR: "Euro",
+  FJD: "Fijian Dollar",
+  FKP: "Falkland Islands Pound",
+  GBP: "Pound Sterling",
+  GEL: "Georgian Lari",
+  GHS: "Ghanaian Cedi",
+  GIP: "Gibraltar Pound",
+  GMD: "Gambian Dalasi",
+  GNF: "Guinean Franc",
+  GTQ: "Guatemalan Quetzal",
+  GYD: "Guyanese Dollar",
+  HKD: "Hong Kong Dollar",
+  HNL: "Honduran Lempira",
+  HRK: "Croatian Kuna",
+  HTG: "Haitian Gourde",
+  HUF: "Hungarian Forint",
+  IDR: "Indonesian Rupiah",
+  ILS: "Israeli New Shekel",
+  INR: "Indian Rupee",
+  IQD: "Iraqi Dinar",
+  IRR: "Iranian Rial",
+  ISK: "Icelandic Króna",
+  JMD: "Jamaican Dollar",
+  JOD: "Jordanian Dinar",
+  JPY: "Japanese Yen",
+  KES: "Kenyan Shilling",
+  KGS: "Kyrgyzstani Som",
+  KHR: "Cambodian Riel",
+  KMF: "Comorian Franc",
+  KPW: "North Korean Won",
+  KRW: "South Korean Won",
+  KWD: "Kuwaiti Dinar",
+  KYD: "Cayman Islands Dollar",
+  KZT: "Kazakhstani Tenge",
+  LAK: "Lao Kip",
+  LBP: "Lebanese Pound",
+  LKR: "Sri Lankan Rupee",
+  LRD: "Liberian Dollar",
+  LSL: "Lesotho Loti",
+  LTL: "Lithuanian Litas (old)",
+  LVL: "Latvian Lats (old)",
+  LYD: "Libyan Dinar",
+  MAD: "Moroccan Dirham",
+  MDL: "Moldovan Leu",
+  MGA: "Malagasy Ariary",
+  MKD: "Macedonian Denar",
+  MMK: "Myanmar Kyat",
+  MNT: "Mongolian Tögrög",
+  MOP: "Macanese Pataca",
+  MRO: "Mauritanian Ouguiya (old)",
+  MUR: "Mauritian Rupee",
+  MVR: "Maldivian Rufiyaa",
+  MWK: "Malawian Kwacha",
+  MXN: "Mexican Peso",
+  MYR: "Malaysian Ringgit",
+  MZN: "Mozambican Metical",
+  NAD: "Namibian Dollar",
+  NGN: "Nigerian Naira",
+  NIO: "Nicaraguan Córdoba",
+  NOK: "Norwegian Krone",
+  NPR: "Nepalese Rupee",
+  NZD: "New Zealand Dollar",
+  OMR: "Omani Rial",
+  PAB: "Panamanian Balboa",
+  PEN: "Peruvian Sol",
+  PGK: "Papua New Guinean Kina",
+  PHP: "Philippine Peso",
+  PKR: "Pakistani Rupee",
+  PLN: "Polish Złoty",
+  PYG: "Paraguayan Guaraní",
+  QAR: "Qatari Riyal",
+  RON: "Romanian Leu",
+  RSD: "Serbian Dinar",
+  RUB: "Russian Ruble",
+  RWF: "Rwandan Franc",
+  SAR: "Saudi Riyal",
+  SBD: "Solomon Islands Dollar",
+  SCR: "Seychellois Rupee",
+  SDG: "Sudanese Pound",
+  SEK: "Swedish Krona",
+  SGD: "Singapore Dollar",
+  SHP: "Saint Helena Pound",
+  SLL: "Sierra Leonean Leone",
+  SOS: "Somali Shilling",
+  SRD: "Surinamese Dollar",
+  STD: "São Tomé and Príncipe Dobra (old)",
+  SYP: "Syrian Pound",
+  SZL: "Swazi Lilangeni",
+  THB: "Thai Baht",
+  TJS: "Tajikistani Somoni",
+  TMT: "Turkmenistani Manat",
+  TND: "Tunisian Dinar",
+  TOP: "Tongan Paʻanga",
+  TRY: "Turkish Lira",
+  TTD: "Trinidad and Tobago Dollar",
+  TWD: "New Taiwan Dollar",
+  TZS: "Tanzanian Shilling",
+  UAH: "Ukrainian Hryvnia",
+  UGX: "Ugandan Shilling",
+  USD: "United States Dollar",
+  UYU: "Uruguayan Peso",
+  UZS: "Uzbekistani Som",
+  VEF: "Venezuelan Bolívar (old)",
+  VND: "Vietnamese Đồng",
+  VUV: "Vanuatu Vatu",
+  WST: "Samoan Tala",
+  XAF: "Central African CFA Franc",
+  XOF: "West African CFA Franc",
+  XPF: "CFP Franc",
+  YER: "Yemeni Rial",
+  ZAR: "South African Rand",
+  ZMK: "Zambian Kwacha (old)",
+  ZWL: "Zimbabwean Dollar"
+};
+
+const dropdownlist = document.querySelectorAll("form select");
+const fromcurrency = document.querySelector("#form ");
+const tocurrency = document.querySelector("#to ");
+const Get_Exchange = document.querySelector("form button");
+const amount = document.querySelector("form input");
+const  Get_Exchange_text= document.querySelector("form .exchange-rate");
+const  Get_Exchange_reverse= document.querySelector(".icon");
+
+for (let index = 0; index < dropdownlist.length; index++) {
+    
+    for (const currency_code in country_list) {
+       let selected =
+            index == 0
+            ? currency_code == "RUB"
+                ? "selected"
+                : ""
+            : currency_code == "INR"
+            ? "selected"
+            : "";
+
+        let option = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;   
+        dropdownlist[index].insertAdjacentHTML("beforeend",option);
+    }
+    dropdownlist[index].addEventListener("change",(e)=>{
+        loadflag(e.target);
+    });
+}
+
+function loadflag(flag){
+    for (const code in country_list) {
+        if(code==flag.value){
+            let imgtag =  flag.parentElement.querySelector("img");
+            imgtag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
+        }
+    }
+}
+
+Get_Exchange.addEventListener("click",(e)=>{
+    e.preventDefault();
+     let value = amount.value.trim();
+    if(value ==''||value=='0')
+    {
+        alert(`Please enter valid amount`);
+        return;
+    }
+    getexchangerate(value);
+});
+Get_Exchange.addEventListener("keydown",(e)=>{
+    if(e.key=="enter")
+    {
+        e.preventDefault();
+        let value = amount.value.trim();
+        if(value ==''||value=='0')
+        {
+            alert(`Please enter valid amount`);
+            return;
+        }
+        getexchangerate(value);
+    }
+});
+
+function getexchangerate(value){
+    Get_Exchange_text.innerText = "Getting exchange rate...";
+    const apikey = `6380ff663445f76176dfc700`;
+    const url = `https://v6.exchangerate-api.com/v6/${apikey}/latest/${fromcurrency.value}`;
+    fetch(url)
+    .then((response)=> response.json())
+    .then((data) =>{
+        console.log(data);
+        let rate = data.conversion_rates[tocurrency.value];
+        let totalrate = (value*rate).toFixed(2);
+        Get_Exchange_text.innerHTML =`${value} ${fromcurrency.value} = ${totalrate} ${tocurrency.value}`; 
+        
+    })
+    .catch( ()=>{
+        Get_Exchange_text.innerHTML = `Something went wrong`;
+    });
+  
+}
+
+Get_Exchange_reverse.addEventListener("click",()=>{
+    let temp = fromcurrency.value;
+    fromcurrency.value = tocurrency.value;
+    tocurrency.value = temp;
+    loadflag(fromcurrency);
+    loadflag(tocurrency);
+    value = amount.value.trim();
+    getexchangerate(value);
+})
